7c290e24219bef2e0c5c0004c68f836cd90a5c18
Satnam <123431496+yosoyss@users.noreply.github.com>
2024-07-27 20:27:21 +0530

    Update kick.js

--- src/kick.js
+++ src/kick.js
@@ -1,4 +1,4 @@
-const client = require('../index.js');
+import client from '../index.js';
 
 /*************my try ban*************/
 client.onText(/^[\/!#]kick$/, async (msg) => {
@@ -6,58 +6,48 @@ client.onText(/^[\/!#]kick$/, async (msg) => {
     let bot = await client.getMe();
     let mem = await client.getChatMember(msg.chat.id, msg.reply_to_message.from.id);
 
-    //check group
-    if (msg.chat.type === "channel" ||
-        msg.chat.type === "supergroup" ||
-        msg.chat.type === "group") {
+    // Check group
+    if (["channel", "supergroup", "group"].includes(msg.chat.type)) {
 
-        //check member perms
-        client.getChatAdministrators(msg.chat.id).then(admins => admins.some(child => child.user.id == msg.from.id)).then(isAdmin => {
-            if (isAdmin) {
-                //check bot perms
-                client.getChatAdministrators(msg.chat.id).then(admins => admins.some(child => child.user.id == bot.id)).then(bisAdmin => {
-                    if (bisAdmin) {
-                        // check if bot got mentioned lol
-                        if (msg.reply_to_message.from.id != bot.id) {
-                            //check if command owner user cmd
-                            if (msg.reply_to_message.from.id != msg.from.id) {
-                                //then kick the member  
-                                client.kickChatMember(msg.chat.id, msg.reply_to_message.from.id)
-                                    .then(() => client.sendMessage(msg.chat.id, msg.reply_to_message.from.first_name + " has been kicked")
-                                    ).catch(e => {
-                                        if (e.response.body.error_code == 400) {
-                                            client.sendMessage(msg.chat.id, "I can't kick admin.");
-                                        }
-                                        // client.deleteMessage(chatId, messageId);
-                                    });
-                                //you cant kick yourself.
+        // Check member permissions
+        client.getChatAdministrators(msg.chat.id)
+            .then(admins => admins.some(child => child.user.id == msg.from.id))
+            .then(isAdmin => {
+                if (isAdmin) {
+                    // Check bot permissions
+                    client.getChatAdministrators(msg.chat.id)
+                        .then(admins => admins.some(child => child.user.id == bot.id))
+                        .then(bisAdmin => {
+                            if (bisAdmin) {
+                                // Check if bot got mentioned
+                                if (msg.reply_to_message.from.id != bot.id) {
+                                    // Check if command owner uses command
+                                    if (msg.reply_to_message.from.id != msg.from.id) {
+                                        // Kick the member  
+                                        client.kickChatMember(msg.chat.id, msg.reply_to_message.from.id)
+                                            .then(() => client.sendMessage(msg.chat.id, `${msg.reply_to_message.from.first_name} has been successfully kicked from the group.`))
+                                            .catch(e => {
+                                                if (e.response.body.error_code == 400) {
+                                                    client.sendMessage(msg.chat.id, "I can't kick an admin. Please demote them first.");
+                                                } else {
+                                                    client.sendMessage(msg.chat.id, "An error occurred while trying to kick the member. Please try again.");
+                                                }
+                                            });
+                                    } else {
+                                        client.sendMessage(msg.chat.id, "You cannot kick yourself!");
+                                    }
+                                } else {
+                                    client.sendMessage(msg.chat.id, "Why would you want to kick me? 😢");
+                                }
                             } else {
-                                client.sendMessage(msg.chat.id, "You cant Kick yourself!")
+                                client.sendMessage(msg.chat.id, "I need administrator privileges to run this command. Please grant me the necessary permissions.");
                             }
-                            //you cant ban me
-                        } else {
-                            client.sendMessage(msg.chat.id, "Why you wanna kick me. ;-;");
-                        }
-
-                        //bot has no perms
-                    } else {
-                        client.sendMessage(msg.chat.id, "I need administrator privileges to run this command!");
-                    }
-                });
-                //if user is not admin
-            } else {
-                client.sendMessage(msg.chat.id, "You need administrator privileges to run this command!");
-            }
-        });
-        //return if mem not found
-        /*  } else {
-            client.sendMessage(msg.chat.id, mem.id + " " + mem.first_name)
-            client.sendMessage(msg.chat.id, "Member not found!")
-          }*/
-
-        //return if isnt a group
+                        });
+                } else {
+                    client.sendMessage(msg.chat.id, "You need administrator privileges to use this command.");
+                }
+            });
     } else {
-        client.sendMessage(msg.chat.id, "This Command doesn't work in private.");
+        client.sendMessage(msg.chat.id, "This command only works in group chats.");
     }
-
-});  
+});
