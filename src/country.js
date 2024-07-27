5538f7484cca0d12e48d499e3095d09433304c68
Satnam <123431496+yosoyss@users.noreply.github.com>
2024-07-27 20:08:07 +0530

    Update country.js

--- src/country.js
+++ src/country.js
@@ -1,34 +1,31 @@
-const client = require('../index.js');
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
+import client from '../index.js';
 
-client.onText(/\/country/, async (message, match) => {
+export default (client) => {
+    client.onText(/\/country/, async (message, match) => {
 
-    let id = message.chat.id;
+        let id = message.chat.id;
 
-    client.sendChatAction(id, "typing");
+        client.sendChatAction(id, "typing");
 
-    //check if args is not null
-    if (match.input === "/country") {
-        client.sendMessage(id, "You haven't provide any argument.");
+        //check if args is not null
+        if (match.input === "/country") {
+            client.sendMessage(id, "You haven't provide any argument.");
+        } else {
+            //search arg
+            let country = match.input.split(' ')[1];
 
-    } else {
+            if (country.toLowerCase() === 'india') {
+                country = country.replace(/india/i, 'bharat');
+            }
 
-        //search arg
-        let country = match.input.split(' ')[1];
+            try {
+                const dataFetch = await fetch(`https://restcountries.com/v2/name/${country}`);
+                const json = await dataFetch.json();
+                const data = json[0];
 
-        if (country.toLowerCase() == 'india') {
-            country = country.replace(/india/i, 'bharat')
-        }
-
-        const dataFetch = await fetch(`https://restcountries.com/v2/name/${country}`)
-
-        const json = await dataFetch.json();
-
-        const data = json[0];
-        try {
-
-            client.sendMessage(id,
-                `${data.name} (${data.nativeName})
+                client.sendMessage(id, 
+                    `${data.name} (${data.nativeName})
 
 • **Capital**: ${data.capital}
 • **Currency**: ${data.currencies[0].code} | ${data.currencies[0].symbol}
@@ -42,11 +39,12 @@ client.onText(/\/country/, async (message, match) => {
 • **Region**: ${data.region} \`[${data.subregion}]\`
 • **TimeZones**: \`${data.timezones}\`
 
-Powered by restcountries.eu`, { parse_mode: "Markdown" }).catch(err => { });
-
-        } catch {
-            return client.sendMessage(id, `Result not found!`);
+Powered by restcountries.eu`, 
+                    { parse_mode: "Markdown" }
+                ).catch(err => { console.error(err); });
+            } catch (error) {
+                client.sendMessage(id, `Result not found!`);
+            }
         }
-        //end
-    }
-});
+    });
+};
