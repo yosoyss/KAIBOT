11f115ab27ad2f395b2d2c135023c1ffee7c2f74
Satnam <123431496+yosoyss@users.noreply.github.com>
2024-07-27 20:28:28 +0530

    Update lockdown.js

--- src/lockdown.js
+++ src/lockdown.js
@@ -1,80 +1,81 @@
-const client = require('../index.js');
+import client from '../index.js';
 
 /**********lockdown**********/
 client.onText(/^[\/!#]lockdown$/, async (msg) => {
-    let perms = {
+    const perms = {
         can_send_messages: false,
         can_send_polls: false,
         can_change_info: false
     };
 
-    let bot = await client.getMe();
+    const bot = await client.getMe();
 
-    //check group
-    if (msg.chat.type === "channel" ||
-        msg.chat.type === "supergroup" ||
-        msg.chat.type === "group") {
+    // Check if the chat is a group or channel
+    if (["channel", "supergroup", "group"].includes(msg.chat.type)) {
 
         client.sendChatAction(msg.chat.id, "typing");
 
-        //check member perms
-        client.getChatAdministrators(msg.chat.id).then(admins => admins.some(child => child.user.id == msg.from.id)).then(isAdmin => {
-            if (isAdmin) {
-                //check bot perms
-                client.getChatAdministrators(msg.chat.id).then(admins => admins.some(child => child.user.id == bot.id)).then(bisAdmin => {
-                    if (bisAdmin) {
-                        client.setChatPermissions(msg.chat.id, perms);
-                        client.sendMessage(msg.chat.id, "Sucessfully locked channel.");
-                    } else {
-                        client.sendMessage(msg.chat.id, "I need administrator privileges to run this command!");
-                    }
-                });
-                //if user is not admin
-            } else {
-                client.sendMessage(msg.chat.id, "You need administrator privileges to run this command!");
-            }
-        });
-        //return if isnt a group
+        // Check if the user has admin privileges
+        client.getChatAdministrators(msg.chat.id)
+            .then(admins => admins.some(child => child.user.id == msg.from.id))
+            .then(isAdmin => {
+                if (isAdmin) {
+                    // Check if the bot has admin privileges
+                    client.getChatAdministrators(msg.chat.id)
+                        .then(admins => admins.some(child => child.user.id == bot.id))
+                        .then(bisAdmin => {
+                            if (bisAdmin) {
+                                client.setChatPermissions(msg.chat.id, perms);
+                                client.sendMessage(msg.chat.id, "Channel successfully locked. Only admins can send messages now.");
+                            } else {
+                                client.sendMessage(msg.chat.id, "I need administrator privileges to lock the channel. Please grant me the necessary permissions.");
+                            }
+                        });
+                } else {
+                    client.sendMessage(msg.chat.id, "You need administrator privileges to lock the channel.");
+                }
+            });
     } else {
-        client.sendMessage(msg.chat.id, "This Command doesn't work in private.");
+        client.sendMessage(msg.chat.id, "This command doesn't work in private chats.");
     }
 });
+
 /**************unlock channel********/
 client.onText(/^[\/!#]unlock$/, async (msg) => {
-    let perms = {
+    const perms = {
         can_send_messages: true,
         can_send_polls: true,
         can_change_info: true
     };
 
-    let bot = await client.getMe();
+    const bot = await client.getMe();
 
-    if (msg.chat.type === "channel" ||
-        msg.chat.type === "supergroup" ||
-        msg.chat.type === "group") {
+    // Check if the chat is a group or channel
+    if (["channel", "supergroup", "group"].includes(msg.chat.type)) {
 
         client.sendChatAction(msg.chat.id, "typing");
 
-        //check member perms
-        client.getChatAdministrators(msg.chat.id).then(admins => admins.some(child => child.user.id == msg.from.id)).then(isAdmin => {
-            if (isAdmin) {
-                //check bot perms
-                client.getChatAdministrators(msg.chat.id).then(admins => admins.some(child => child.user.id == bot.id)).then(bisAdmin => {
-                    if (bisAdmin) {
-                        client.setChatPermissions(msg.chat.id, perms);
-                        client.sendMessage(msg.chat.id, "Sucessfully unlocked channel.");
-                    } else {
-                        client.sendMessage(msg.chat.id, "I need administrator privileges to run this command!");
-                    }
-                });
-                //if user is not admin
-
-            } else {
-                client.sendMessage(msg.chat.id, "You need administrator privileges to run this command!");
-            }
-        });
-        //if not a group
+        // Check if the user has admin privileges
+        client.getChatAdministrators(msg.chat.id)
+            .then(admins => admins.some(child => child.user.id == msg.from.id))
+            .then(isAdmin => {
+                if (isAdmin) {
+                    // Check if the bot has admin privileges
+                    client.getChatAdministrators(msg.chat.id)
+                        .then(admins => admins.some(child => child.user.id == bot.id))
+                        .then(bisAdmin => {
+                            if (bisAdmin) {
+                                client.setChatPermissions(msg.chat.id, perms);
+                                client.sendMessage(msg.chat.id, "Channel successfully unlocked. Members can send messages now.");
+                            } else {
+                                client.sendMessage(msg.chat.id, "I need administrator privileges to unlock the channel. Please grant me the necessary permissions.");
+                            }
+                        });
+                } else {
+                    client.sendMessage(msg.chat.id, "You need administrator privileges to unlock the channel.");
+                }
+            });
     } else {
-        client.sendMessage(msg.chat.id, "This Command doesn't work in private.");
+        client.sendMessage(msg.chat.id, "This command doesn't work in private chats.");
     }
 });
