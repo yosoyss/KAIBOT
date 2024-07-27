58fc6d7ac63f0455db1ddfdd5ce702141b754936
Satnam <123431496+yosoyss@users.noreply.github.com>
2024-07-27 20:21:34 +0530

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,53 +1,52 @@
-const client = require('../index.js');
+import client from '../index.js';
 
 /*************my try ban*************/
 client.on("message", async (msg) => {
-      if(msg && msg.text && typeof msg.text === "string" && msg.text.startsWith("@kaiweb_bot")){
+    if (msg && msg.text && typeof msg.text === "string" && msg.text.startsWith("@kaiweb_bot")) {
         client.sendMessage(msg.chat.id, "Write /help to see my command list!");
-      }
+    }
 });
-client.onText(/^[\/!#]help/, async (msg) => {
-    
 
-  let data = [
+client.onText(/^[\/!#]help/, async (msg) => {
+    let data = [
 `*Client Commands:*
 /stats → Some of my details.
 /help → This embed.
 
 *Moderation Commands:* 
 /lockdown → To lock the channel for all members.
-/unlock → To unlock  a channel.
-/kick → To kick useless user from server.
+/unlock → To unlock a channel.
+/kick → To kick a useless user from the server.
 /mute → To mute anyone.
-/unmute → To unmute mute members.
-/pin → pins message of the group.
-/chatTitle → sets the title of the group.
+/unmute → To unmute muted members.
+/pin → Pins message of the group.
+/chatTitle → Sets the title of the group.
 
 *Music Commands:* 
-/song → sends a audio file of the song.
-/lyrics → shows the lyrics of song.
+/song → Sends an audio file of the song.
+/lyrics → Shows the lyrics of the song.
 
 *General Commands:* 
 /avatar → Shows your profile picture.
 /say → To call something from the bot.
-/admins → shows list of the server admins.
-/meme → return random memes.
+/admins → Shows list of the server admins.
+/meme → Returns random memes.
 
 *Game Commands:* 
-/trivia  → Test Your skills.  
+/trivia → Test your skills.
 
 *Animal Commands:* 
-/dog → Sends a random  dog pics 
-/cat → Sends a random  cat pics.
+/dog → Sends a random dog pic
+/cat → Sends a random cat pic.
 
 *Google Commands:*    
 /urban → To search for the meaning of a word.
-/country → shows the info of the countries.
+/country → Shows the info of the countries.
 /weather → To know the weather information of any location.
-/currency → shows currency exchanges.`
-];
-client.sendChatAction(msg.chat.id, 'typing');
-
-  client.sendMessage(msg.chat.id, data[0], {parse_mode : "Markdown"});
-  
+/currency → Shows currency exchanges.`
+    ];
+    client.sendChatAction(msg.chat.id, 'typing');
+    client.sendMessage(msg.chat.id, data[0], { parse_mode: "Markdown" });
 });
+
+export default (client);
