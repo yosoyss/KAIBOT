08082a4006813b2acdf5a884ed7a64e333c025c5
Satnam <123431496+yosoyss@users.noreply.github.com>
2024-07-27 20:32:41 +0530

    Update stats.js

--- src/stats.js
+++ src/stats.js
@@ -1,27 +1,34 @@
+import client from '../index.js';
+import ms from 'ms';
+import { mem, cpu, os } from 'node-os-utils';
 
-const client = require('../index.js');
-const ms = require('ms');
-const { mem, cpu, os } = require('node-os-utils');
-
-/************main code start*************/
+// Handle the /stats command
 client.onText(/^[\/!#]stats$/, async msg => {
+    try {
+        client.sendChatAction(msg.chat.id, 'typing');
 
-    ///if(msg.from.id == process.env.dev){
-    client.sendChatAction(msg.chat.id, "typing");
-    const osCount = await cpu.usage()
-    const memUsage = parseFloat(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2);
-    const totalMem = parseFloat(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2);
+        const osType = os.type();
+        const osUptime = ms(os.uptime());
+        const cpuUsage = await cpu.usage();
+        const memUsage = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2);
+        const totalMem = (process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2);
+        const osVersion = await os.oos();
 
-    client.sendMessage(msg.chat.id,
-        `
-Bot Status :
+        const response = `
+*Bot Status:*
 
-• OS Type : ${os.type()}
-• OS OSS : ${await os.oos()}
-• Memory Usage : ${memUsage} MB
-• Total Memory : ${totalMem} MB 
-• Cpu Usage : ${osCount}%
+• OS Type: ${osType}
+• OS Version: ${osVersion}
+• Memory Usage: ${memUsage} MB
+• Total Memory: ${totalMem} MB
+• CPU Usage: ${cpuUsage}%
+• OS Uptime: ${osUptime}
+`;
 
-OS Uptime : ${ms(os.uptime())}`, { parse_mode: "Markdown" });
-    // }
+        await client.sendMessage(msg.chat.id, response, { parse_mode: 'Markdown' });
+
+    } catch (err) {
+        console.error('Error handling /stats command:', err);
+        await client.sendMessage(msg.chat.id, 'An error occurred while fetching the stats. Please try again later.');
+    }
 });
